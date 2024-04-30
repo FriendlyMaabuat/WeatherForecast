2016a59ccca940fa470a75d30843edcbb5dde31c
FriendlyMaabuat <friendlymaabuat25@gmail.com>
2024-04-30 13:13:41 +0800

    Plotting dummy data on a line graph using Plotly

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the number of forecasted days")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} in {place}")
+
+
+def get_data(days):
+    dates = ["01-01-2024", "02-01-2024", "03-01-2024"]
+    temperatures= [24, 25, 27]
+    temperatures = [days * i for i in temperatures]
+    return dates, temperatures
+
+d, t = get_data(days)
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
 
